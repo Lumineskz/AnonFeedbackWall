27ff8a326236722627a93e120bff9df0aa0ba833
Lumineskz <shubhamsulabh6@gmail.com>
2025-11-18 09:52:55 +0545

    Changed top header design

--- Code/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Replace 'YOUR_ADMIN_PASSWORD_HERE' with the actual password you want.
-$password = 'admin123'; 
-
-// PASSWORD_DEFAULT uses the strongest algorithm (currently bcrypt)
-$hash = password_hash($password, PASSWORD_DEFAULT); 
-echo $hash . "\n";
-?>
